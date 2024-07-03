04a0c5e9356262732efa51607a1f9589692b9a0a
lab900-johan-cosemans <johan.cosemans@lab900.com>
2024-07-03 22:50:51 +0200

    initial commit

--- sprinklers.py
+++ sprinklers.py
@@ -10,8 +10,22 @@ def log_hello_world():
     log.info("Getting status of the sprinklers on ip " + ESP32_SPRINKLERS_IP)
     url = f'http://{ESP32_SPRINKLERS_IP}/status.xml'
     statusResponseXml = task.executor(requests.get, url)
+#<?xml version="1.0" encoding="UTF-8"?>
+#<ESP32LR42DATA>
+#<RELAYS>
+#<RLY1>on</RLY1>
+#<RLY2>on</RLY2>
+#<RLY3>on</RLY3>
+#<RLY4>on</RLY4>
+#</RELAYS>
+#<INPUTS>
+#<INP1>1</INP1>
+#<INP2>1</INP2>
+#</INPUTS>
+#</ESP32LR42DATA>
     statusResponse = xmltodict.parse(statusResponseXml.text)
-    log.info(statusResponse)
+#    log.info(statusResponse)
+    log.info(statusResponse['ESP32LR42DATA']['RELAYS']['RLY1'])
 
 
 
