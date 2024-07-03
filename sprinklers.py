a903efc2a9973945e8a29bf65c86c8cfa17ce06d
lab900-johan-cosemans <johan.cosemans@lab900.com>
2024-07-03 22:06:35 +0200

    initial commit

--- sprinklers.py
+++ sprinklers.py
@@ -1,9 +1,5 @@
+import os
+
 @service
 def log_hello_world():
-    log.info(f"hello world!")
-
-    
-@time_trigger('period(now, 10s)')
-def run_get_info():
-    log.debug('running get_station_info')
-    
+    log.info("Hello World")
