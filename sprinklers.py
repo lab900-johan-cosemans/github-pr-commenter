ec45c473e442cd5190a578a4960a33eb6bc180f0
root <root@core-configurator>
2024-07-03 22:02:55 +0200

    e

--- sprinklers.py
+++ sprinklers.py
@@ -1,3 +1,8 @@
-@time_trigger('period(now, 30s)')
+
+log.debug('running get_station_info')
+
+    
+@time_trigger('period(now, 10s)')
 def run_get_info():
     log.debug('running get_station_info')
+    
